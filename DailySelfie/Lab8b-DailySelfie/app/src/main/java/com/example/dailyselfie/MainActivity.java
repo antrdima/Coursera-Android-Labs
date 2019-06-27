988622f6393bd6261c7638378788f520ec49248e
Dmitry <antrdima@gmail.com>
2019-06-27 14:54:19 +0500

    Add take photo icon on toolbar

--- DailySelfie/Lab8b-DailySelfie/app/src/main/java/com/example/dailyselfie/MainActivity.java
+++ DailySelfie/Lab8b-DailySelfie/app/src/main/java/com/example/dailyselfie/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.dailyselfie;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -19,4 +22,27 @@ public class MainActivity extends AppCompatActivity {
         actionBar.setIcon(R.mipmap.ic_launcher);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        MenuItem takePhotoItem = menu.findItem(R.id.action_take_photo);
+
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.action_take_photo:
+                TakePhoto();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void TakePhoto() {
+    }
 }
